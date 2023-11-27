7acb3751e362a4624a65ac11c15c1bb235959b7b
iffigues <bordenoy@student.42.fr>
2023-11-27 15:30:10 +0100

    create onError function for catch error and exit programme if have error

--- deembed.go
+++ deembed.go
@@ -2,20 +2,32 @@ package main
 
 import (
 	"deembed/macho"
+	"errors"
 	"fmt"
 	"os"
 )
 
-func getPlatform(a string) exec {
+func onError(err error, e int, a ...string) {
+	if err == nil {
+		return
+	}
+	for _, val := range a {
+		fmt.Print(val + "\n")
+	}
+	fmt.Println(err)
+	os.Exit(e)
+}
+
+func getPlatform(a string) (exec, error) {
 	switch a {
 	case "windows":
-		return nil
+		return nil, nil
 	case "darwin":
-		return &macho.Macho{}
+		return &macho.Macho{}, nil
 	case "linux":
-		return nil
+		return nil, nil
 	default:
-		return nil
+		return nil, errors.New("operating system " + a + " is not gerer")
 	}
 }
 
@@ -25,16 +37,9 @@ func start(e exec, a string) {
 
 func deembed(a string) {
 	bi, err := getBuildInfo(a)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	onError(err, 1)
 	goos := getBuildInfoSetting(bi, "GOOS")
-	exe := getPlatform(goos)
-	if exe == nil {
-		fmt.Print("operating system ", goos, " is not gerer\n")
-		os.Exit(1)
-	}
+	exe, err := getPlatform(goos)
+	onError(err, 1)
 	start(exe, a)
-
 }
